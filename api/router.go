35a841286354d193a5540e8bfa539bcf07598be2
Ozodbek <ozodbekshamsiddinov1504@gmail.com>
2025-11-18 16:50:53 +0500

    all

--- api/router.go
+++ api/router.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"rastochnoy/api/handler"
 	writerastochnoy "rastochnoy/write_rastochnoy"
 
@@ -10,13 +11,15 @@ import (
 func RegisterRastochnoyRoutes(h *handler.Handler) *gin.Engine {
 	router := gin.Default()
 
+	router.Use(CORSMiddleware())
+
 	r := router.Group("/rastochnoy")
 	{
 		r.PUT("/write", h.WriteRastochnoy)
 		r.GET("/readwrite", h.ReadWriteRastochnoy)
 		r.GET("/read", h.ReadRastochnoy)
 		r.GET("/ws", writerastochnoy.WebSocketHandler) // ✅ WebSocket endpoint
-		r.GET("/wsdb9",writerastochnoy.WebSocketHandlerDB9)
+		r.GET("/wsdb9", writerastochnoy.WebSocketHandlerDB9)
 		r.PUT("/writeRastochnoy_db37", h.WrtieRastochnoy_db37)
 		r.PUT("/writedb33", h.WriteRastochnoyDB33)
 		r.GET("/readwritedb33", h.ReadWriteRastochnoyDB33)
@@ -24,4 +27,22 @@ func RegisterRastochnoyRoutes(h *handler.Handler) *gin.Engine {
 	}
 
 	return router
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Println("Cors middleware triggered")
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
